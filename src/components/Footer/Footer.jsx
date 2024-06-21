0015ba5174bc86fc0b1cdee5d9f4a89ffa596e28
Henrique <venoshk.henrique19@gmail.com>
2024-06-21 14:41:57 -0300

    Adicionando radapé

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import Logo from "../../assets/images/logo.svg"
+export const Footer = () => {
+  return (
+    <footer className="bg-Footer flex flex-col items-center md:items-start justify-center md:justify-around gap-5 rounded-borderRadiusMobileFooter md:flex-row w-full py-10">
+        <div>
+            <img src={Logo} alt="" />
+        </div>
+        
+
+        <div className="flex flex-col text-center md:text-start">
+            <a href="#" className="text-white font-bold mb-5">Product</a>
+            <a href="#" className="text-colorVeryDark font-medium hover:text-white transition-colors">Overview</a>
+            <a href="#" className="text-colorVeryDark font-medium hover:text-white transition-colors">Princing</a>
+            <a href="#" className="text-colorVeryDark font-medium hover:text-white transition-colors">Marketplace</a>
+            <a href="#" className="text-colorVeryDark font-medium hover:text-white transition-colors">Features</a>
+            <a href="#" className="text-colorVeryDark font-medium hover:text-white transition-colors">Integrations</a>
+        </div>
+
+        <div className="flex flex-col text-center md:text-start">
+            <a href="#" className="text-white font-bold mb-5">Company</a>
+            <a href="#" className="text-colorVeryDark font-medium hover:text-white transition-colors">About</a>
+            <a href="#" className="text-colorVeryDark font-medium hover:text-white transition-colors">Team</a>
+            <a href="#" className="text-colorVeryDark font-medium hover:text-white transition-colors">Blog</a>
+            <a href="#" className="text-colorVeryDark font-medium hover:text-white transition-colors">Careers</a>
+        </div>
+
+        <div className="flex flex-col text-center md:text-start">
+            <a href="#" className="text-white font-bold mb-5">Connect</a>
+            <a href="#" className="text-colorVeryDark font-medium hover:text-white transition-colors">Newsletter</a>
+            <a href="#" className="text-colorVeryDark font-medium hover:text-white transition-colors">Linkedln</a>
+        </div>
+    </footer>
+  )
+}
